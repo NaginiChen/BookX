d3b9c02bd1acc13edf42469461474c2d6930091c
Kelly Ruan <kruan@bu.edu>
2020-04-13 01:46:16 -0400

    user and listing database

---
+++ app/src/main/java/com/example/bookx/AccountsActivity.java
@@ -0,0 +1,6 @@
+package com.example.bookx;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AccountsActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/example/bookx/ListingsActivity.java
@@ -0,0 +1,4 @@
+package com.example.bookx;
+
+public class ListingsActivity {
+}
