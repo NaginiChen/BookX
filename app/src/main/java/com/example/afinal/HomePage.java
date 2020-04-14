26ab9cd37e00251a76bf2bbba3c4c5d22c6cea93
TTTheo <wart_carson@yeah.net>
2020-04-14 17:29:46 -0400

    simple custom listview added

--- app/src/main/java/com/example/afinal/HomePage.java
+++ app/src/main/java/com/example/afinal/HomePage.java
@@ -6,13 +6,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import com.example.afinal.Model.Post;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomePage extends AppCompatActivity {
 
     TextView title_tv;
     Button account_btn;
     Button preferences_btn;
+    private List<Post> posts = new ArrayList<>();
+    private ListView lvPosts ;
+    private ListAdapter postAdapter ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +47,12 @@ public class HomePage extends AppCompatActivity {
                 openPreferencesPage();
             }
         });
+
+        data();
+        postAdapter = new listingAdapter(this.getBaseContext(),posts) ;
+        lvPosts = (ListView) findViewById(R.id.lvListing) ;
+        lvPosts.setAdapter(postAdapter);
+        lvPosts.setItemsCanFocus(true);
     }
     public void openAccountPage() {
         Intent intent = new Intent(this, AccountPage.class);
@@ -47,5 +63,14 @@ public class HomePage extends AppCompatActivity {
         Intent intent = new Intent(this, PreferencesPage.class);
         startActivity(intent);
 
+    }
+
+    public void data(){
+        Post post1 = new Post("book1","seller1","course1",1,"This is book1",false) ;
+        Post post2 = new Post("book2","seller2","course2",2,"This is book2",false) ;
+        Post post3 = new Post("book3","seller3","course3",3,"This is book3",false) ;
+        posts.add(post1) ;
+        posts.add(post2) ;
+        posts.add(post3) ;
     }
 }
