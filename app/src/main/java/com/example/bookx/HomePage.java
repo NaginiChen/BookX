751cf7070a0657673a720e292a99464d9cf2307d
NaginiChen <naginichen@gmail.com>
2020-04-23 19:24:04 -0400

    notifications

--- app/src/main/java/com/example/bookx/HomePage.java
+++ app/src/main/java/com/example/bookx/HomePage.java
@@ -1,8 +1,15 @@
 package com.example.bookx;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -12,10 +19,12 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.bookx.Model.Chat;
 import com.example.bookx.Model.Post;
 import com.example.bookx.Model.User;
 import com.example.bookx.R;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -29,7 +38,7 @@ import java.util.List;
 import java.util.Map;
 
 public class HomePage extends AppCompatActivity {
-    private static final String TAG = "***HOME***";
+    private static final String TAG = "myB";
 
     TextView title_tv;
     Button account_btn;
@@ -43,6 +52,8 @@ public class HomePage extends AppCompatActivity {
     // firebase instance variables
     private FirebaseAuth mAuth;
     private DatabaseReference mDatabase;
+
+    Boolean firstBootUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -93,6 +104,9 @@ public class HomePage extends AppCompatActivity {
                 openListingPage();
             }
         });
+
+        firstBootUp = true;
+        listenForNewMessages();
     }
 
     public void openAccountPage() {
@@ -155,5 +169,76 @@ public class HomePage extends AppCompatActivity {
             }
         });
     }
-}
 
+    // listens for incoming messages
+    private void listenForNewMessages(){
+        mDatabase.child("chats").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String prevChildKey) {
+                final Chat chatVal = dataSnapshot.getValue(Chat.class);
+
+                mDatabase.child("users").addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+                            User user = snapshot.getValue(User.class) ;
+                            if( snapshot.getKey().equals(chatVal.getSender()) && chatVal.getReceiver().equals(mAuth.getUid()) ){
+                                    sendNotification(chatVal.getMessage(), user.getFullName() );
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                    }
+                });
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String prevChildKey) {}
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {}
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String prevChildKey) {}
+            @Override
+            public void onCancelled(DatabaseError databaseError) {}
+        });
+
+    }
+
+    private void sendNotification(String message, String sender){
+        createNotificationChannel();
+        Intent intent = new Intent(this, MessageList.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "1")
+                .setSmallIcon(R.drawable.notification_icon)
+                .setContentTitle("Message from " + sender)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        // notificationId is a unique int for each notification that you must define
+        int notificationId = 1;
+        notificationManager.notify(notificationId, builder.build());
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Messages";
+            String description = "Messages from other users";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("1", name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+}
